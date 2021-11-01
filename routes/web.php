f40566cb37f8244539b15da22967c181114ab98d
muhohoweb@gmail.com <muhohoweb@gmail.com>
2021-11-02 02:22:07 +0300

    disable register

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes();
+Auth::routes(['register'=>false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
