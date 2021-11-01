fb3cc5388f05753d0078074748f511412e2cda3a
muhohoweb@gmail.com <muhohoweb@gmail.com>
2021-11-02 02:19:14 +0300

    enable register

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes(['register'=>false]);
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
